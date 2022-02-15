4bcd7142b2a1d85d823a494899c279661c8ed2a2
EricksonNg <52866215+EricksonNg@users.noreply.github.com>
2022-02-15 15:40:37 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    radio.sendNumber(1)
-    basic.showIcon(IconNames.Heart)
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendNumber(2)
+    basic.showIcon(IconNames.Asleep)
 })
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    basic.showIcon(IconNames.House)
+    radio.sendValue("both", 3)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P3, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     radio.sendNumber(3)
-    basic.showIcon(IconNames.Angry)
+    basic.showIcon(IconNames.Happy)
 })
 radio.setGroup(158)
-radio.sendNumber(0)
-basic.forever(function () {
-	
-})
